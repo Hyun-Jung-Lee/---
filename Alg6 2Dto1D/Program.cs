d7939c12c756ff5e19291efcbfc4b41d4072d9d7
Yuyang Sun <syuyangok@gmail.com>
2019-08-22 11:54:51 -0500

    Need to fix bugs

---
+++ Alg6 2Dto1D/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alg6_2Dto1D
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] m = new[]
+            {
+                new[]{1,2,3,4},
+                new[]{10,11,12,5},
+                new[]{9,8,7,6}
+            };
+
+           //Console.WriteLine(m[0].Length);
+            
+           for(int i=0; i< m.Length; i++)
+            {
+                for(int j=0; j<m[i].Length; j++)
+                {
+                    Console.Write(m[i][j]+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+
+            var result = Solution.Convert(m);
+            Console.WriteLine(String.Join("->", result));
+
+        }//Main end
+    }
+}
---
+++ Alg6 2Dto1D/Solution.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alg6_2Dto1D
+{
+    class Solution
+    {
+        public static int[] Convert(int[][] m)
+        {
+            //check if m is empty
+            if (m == null) return new int[0];
+            
+            // start row and col of converted
+            int row = 0, col = 0;
+            //set area of converted high and wide
+            int high = m.Length, wide = m[0].Length;
+            int p = 0;
+            int[] result=new int[high*wide];
+
+            while (true)
+            {
+                //exit condition
+                if (high == 0 || wide == 0) break;
+                //covernt the top row
+                for(int c=col; c < col + wide; c++)
+                {
+                    result[p] = m[row][c];
+                    p++;
+                }
+                row = row + 1;//after coverted one row, start row number increase 1
+                high = high - 1;
+
+
+                //exit condition
+                if (high == 0 || wide == 0) break;
+                //covernt the right col
+                for (int r = row; r < row + high; r++)
+                {
+                    result[p] = m[r][col+wide -1];
+                    p++;
+                }              
+                wide = wide - 1;
+
+                //exit condition
+                if (high == 0 || wide == 0) break;
+                //covernt the bottom row
+                for (int c = col+wide-1; c > col; c--)
+                {
+                    result[p] = m[row+high-1][c];
+                    p++;
+                }
+
+                //exit condition
+                if (high == 0 || wide == 0) break;
+                //covernt the left col
+                for (int r = row + high - 1; r >= row; r--)
+                {
+                    result[p] = m[r][col];
+                    p++;
+                }
+                col = col + 1;
+                wide = wide - 1;
+            }
+            
+            return result;
+        }
+    }
+}
