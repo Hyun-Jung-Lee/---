aa7a276b9fb7bbe6f7f830a9a6c2810215ee011b
Yuyang Sun <syuyangok@gmail.com>
2020-02-28 11:41:58 -0600

    Add files via upload

---
+++ Alg9BigBomb/Program.cs
@@ -0,0 +1,52 @@
+﻿//Use BFS to search denote all the bombs
+//Then check how many bombs left
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+
+namespace AlgBigBomb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] field =
+            {
+                new[]{0,1,0,0,0,0},
+                new[]{0,1,0,0,0,0},
+                new[]{1,1,1,1,0,0},
+                new[]{0,0,1,0,0,0},
+                new[]{0,1,1,1,1,1},
+                new[]{1,1,1,1,1,1}
+
+            };
+            //check field fist
+            View(field);
+            //denote bomb
+            var solution = new Solution();
+            var counter = solution.DeBomb(field, 0, 3);
+
+            //Console.WriteLine(counter);
+            Console.WriteLine("Bombs left: {0} ", counter);
+            Console.WriteLine();
+            View(field);
+
+        }
+
+        static void View(int[][] field)
+        {
+            foreach(var r in field)
+            {
+                Console.WriteLine(String.Join("", r));
+                
+            }
+        }
+    }
+}
---
+++ Alg9BigBomb/Solution.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgBigBomb
+{
+    class Solution
+    {
+
+        public int DeBomb(int[][] field, int row, int col)
+        {
+            int h = field.Length, w = field[0].Length;
+            int[][] directions =
+            {
+                new[]{-1,0},//up neighbor
+                new[]{1,0}, // down neighbor
+                new[]{0,-1}, //left neighbor
+                new[]{0,1}
+            };
+
+            field[row][col] = 0; // where to place bomb, it will change to zero
+            //use BFS to find 
+            var q = new Queue<int>();
+            q.Enqueue(row);
+            q.Enqueue(col);
+            while (q.Count > 0)
+            {
+                // dequeue center item first
+                int cr = q.Dequeue();
+                int cc = q.Dequeue();
+
+                // get neighbor items
+                foreach (var dir in directions)
+                {
+                    int nr = cr + dir[0];
+                    int nc = cc + dir[1];
+
+                    //check neighbor and enqueue, 
+                    if (nr >= 0 && nr < h && nc >= 0 && nc < w)
+                    {
+                        //if neighbor is also a bomb
+                        if (field[nr][nc] == 1)
+                        {
+                            field[nr][nc] = 0;
+                            q.Enqueue(nr);
+                            q.Enqueue(nc);
+                        }
+                    }
+
+                }
+
+            }//while loop end
+
+            //after denote all the bomb, check how many bombs left
+            int counter = 0;
+            for (int r = 0; r < h; r++)
+            {
+                for (int c = 0; c < w; c++)
+                {
+                    //loop the 2-d array
+                    counter = counter + field[r][c];//if is  bomb, it increase 1, otherwise increase 0, 
+
+                }
+            }
+            return counter;
+
+        }
+
+
+
+
+
+
+
+
+    }
+}
